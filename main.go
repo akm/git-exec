8ba818955ef1844e3cd19b4bf51bf8893345842c
akm <akm2000@gmail.com>
2024-10-03 08:29:44 +0900

    ✨ Initial implementation

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	// 1. このプログラムに渡された引数をコマンドとして実行する。
+	//    その際には、コマンドの照準出力と標準エラー出力をバッファに格納する。
+	// 2. "git add ." を実行し、コマンドによって作成・変更されたカレントディレクトリ以下のファイルを staging area に追加する。
+	// 3. "git commit" を以下のオプションと標準力を指定して実行する。
+	//    オプション : --file -
+	//    標準入力: "🤖 (実行したコマンド)\n\n(バッファ)"
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: git-exec <command>")
+		return
+	}
+
+	// 1. このプログラムに渡された引数をコマンドとして実行する。
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	var outBuf bytes.Buffer
+	cmd.Stdout = &outBuf
+	cmd.Stderr = &outBuf
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Command execution failed: %+v\n", err)
+		return
+	}
+
+	// 2. "git add ." を実行し、コマンドによって作成・変更されたカレントディレクトリ以下のファイルを staging area に追加する。
+	if err := exec.Command("git", "add", ".").Run(); err != nil {
+		fmt.Printf("git add failed: %+v\n", err)
+		return
+	}
+
+	// 3. "git commit" を以下のオプションと標準力を指定して実行する。
+	commitMessage := fmt.Sprintf("🤖 %s\n\n%s\n", strings.Join(os.Args[1:], " "), outBuf.String())
+	// See https://tracpath.com/docs/git-commit/
+	commitCmd := exec.Command("git", "commit", "--file", "-")
+	commitCmd.Stdin = bytes.NewBufferString(commitMessage)
+
+	if err := commitCmd.Run(); err != nil {
+		fmt.Printf("git commit failed: %+v\n", err)
+		return
+	}
+}
