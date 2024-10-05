5e3aa53c37ef36ae54c8ba2b75429058d93807d9
akm <akm2000@gmail.com>
2024-10-05 19:29:39 +0900

    ✅ Add tests for splitArgsToEnvsAndCommand

---
+++ command_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitArgsToEnvsAndCommand(t *testing.T) {
+	patterns := []struct {
+		args        []string
+		envs        []string
+		commandArgs []string
+	}{
+		{
+			[]string{"key1=val1", "key2=val2", "command", "--arg1", "arg2"},
+			[]string{"key1=val1", "key2=val2"},
+			[]string{"command", "--arg1", "arg2"},
+		},
+		{
+			[]string{"command", "--arg1", "arg2"},
+			nil,
+			[]string{"command", "--arg1", "arg2"},
+		},
+	}
+
+	for i, ptn := range patterns {
+		t.Run(fmt.Sprintf("pattern %d", i), func(t *testing.T) {
+			envs, commandArgs := splitArgsToEnvsAndCommand(ptn.args)
+			assert.Equal(t, ptn.envs, envs)
+			assert.Equal(t, ptn.commandArgs, commandArgs)
+		})
+	}
+
+}
