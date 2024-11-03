ac4b794daa5eddefca1ddf0243b8b542db9241d0
akm <akm2000@gmail.com>
2024-11-03 19:13:13 +0900

    ✨ Move --help and --version at the end of options

--- options.go
+++ options.go
@@ -75,8 +75,6 @@ var defaultOptions = &Options{
 }
 
 var (
-	optHelp      = newOptionType("-h", "--help", false, func(o *Options, _ string) { o.Help = true }).withoutEnv()
-	optVersion   = newOptionType("-v", "--version", false, func(o *Options, _ string) { o.Version = true }).withoutEnv()
 	optDirectory = newOptionType("-C", "--directory", true, func(o *Options, v string) { o.Directory = v }).withoutEnv()
 	optEmoji     = newOptionType("-e", "--emoji", true, func(o *Options, v string) { o.Emoji = v })
 	optPrompt    = newOptionType("-p", "--prompt", true, func(o *Options, v string) { o.Prompt = v })
@@ -85,11 +83,12 @@ var (
 	optSkipGuard                   = newOptionType("", "--skip-guard", false, func(o *Options, _ string) { o.SkipGuard = true })
 	optSkipGuardUncommittedChanges = newOptionType("", "--skip-guard-uncommitted-changes", false, func(o *Options, _ string) { o.SkipGuardUncommittedChanges = true })
 	optSkipGuardUntrackedFiles     = newOptionType("", "--skip-guard-untracked-files", false, func(o *Options, _ string) { o.SkipGuardUntrackedFiles = true })
+
+	optHelp    = newOptionType("-h", "--help", false, func(o *Options, _ string) { o.Help = true }).withoutEnv()
+	optVersion = newOptionType("-v", "--version", false, func(o *Options, _ string) { o.Version = true }).withoutEnv()
 )
 
 var optionTypes = []*OptionType{
-	optHelp,
-	optVersion,
 	optDirectory,
 	optEmoji,
 	optPrompt,
@@ -97,6 +96,8 @@ var optionTypes = []*OptionType{
 	optSkipGuard,
 	optSkipGuardUncommittedChanges,
 	optSkipGuardUntrackedFiles,
+	optHelp,
+	optVersion,
 }
 
 var optionKeyMap = func() map[string]*OptionType {
