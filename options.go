53d11ab9e3ae0b6436e72c9fa9fd3f865986b40a
akm <akm2000@gmail.com>
2024-12-07 23:35:45 +0900

    ♻️ Remove variables for optionTypes

--- options.go
+++ options.go
@@ -40,36 +40,24 @@ func strOpt(shortName, longName string, setFunc func(*Options, string)) *opts.De
 	return opts.NewDefinition(envKeyPrefix, shortName, longName, true, setFunc)
 }
 
-var (
-	optDirectory = strOpt("-C", "--directory", func(o *Options, v string) { o.Directory = v }).WithoutEnv()
-	optEmoji     = strOpt("-e", "--emoji", func(o *Options, v string) { o.Emoji = v })
-	optPrompt    = strOpt("-p", "--prompt", func(o *Options, v string) { o.Prompt = v })
-	optTemplate  = strOpt("-t", "--template", func(o *Options, v string) { o.Template = v })
+var optionTypes = func() []*opts.Definition[Options] {
+	return []*opts.Definition[Options]{
+		strOpt("-C", "--directory", func(o *Options, v string) { o.Directory = v }).WithoutEnv(),
+		strOpt("-e", "--emoji", func(o *Options, v string) { o.Emoji = v }),
+		strOpt("-p", "--prompt", func(o *Options, v string) { o.Prompt = v }),
+		strOpt("-t", "--template", func(o *Options, v string) { o.Template = v }),
 
-	optSkipGuard                   = boolOpt("", "--skip-guard", func(o *Options) { o.SkipGuard = true })
-	optSkipGuardUncommittedChanges = boolOpt("", "--skip-guard-uncommitted-changes", func(o *Options) { o.SkipGuardUncommittedChanges = true })
-	optSkipGuardUntrackedFiles     = boolOpt("", "--skip-guard-untracked-files", func(o *Options) { o.SkipGuardUntrackedFiles = true })
+		boolOpt("", "--skip-guard", func(o *Options) { o.SkipGuard = true }),
+		boolOpt("", "--skip-guard-uncommitted-changes", func(o *Options) { o.SkipGuardUncommittedChanges = true }),
+		boolOpt("", "--skip-guard-untracked-files", func(o *Options) { o.SkipGuardUntrackedFiles = true }),
 
-	optDebugLog    = boolOpt("-D", "--debug-log", func(o *Options) { o.DebugLog = true })
-	optInteractive = boolOpt("-i", "--interactive", func(o *Options) { o.Interactive = true })
+		boolOpt("-D", "--debug-log", func(o *Options) { o.DebugLog = true }),
+		boolOpt("-i", "--interactive", func(o *Options) { o.Interactive = true }),
 
-	optHelp    = boolOpt("-h", "--help", func(o *Options) { o.Help = true }).WithoutEnv()
-	optVersion = boolOpt("-v", "--version", func(o *Options) { o.Version = true }).WithoutEnv()
-)
-
-var optionTypes = []*opts.Definition[Options]{
-	optDirectory,
-	optEmoji,
-	optPrompt,
-	optTemplate,
-	optSkipGuard,
-	optSkipGuardUncommittedChanges,
-	optSkipGuardUntrackedFiles,
-	optDebugLog,
-	optInteractive,
-	optHelp,
-	optVersion,
-}
+		boolOpt("-h", "--help", func(o *Options) { o.Help = true }).WithoutEnv(),
+		boolOpt("-v", "--version", func(o *Options) { o.Version = true }).WithoutEnv(),
+	}
+}()
 
 func newOptions() *Options {
 	return opts.NewOptions(optionTypes, defaultOptions)
