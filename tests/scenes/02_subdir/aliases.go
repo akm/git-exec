7840fa70c74e45cb273d2c60727770b4e115902a
akm <akm2000@gmail.com>
2024-12-08 16:57:10 +0900

    ✅ Add directory_option_test.go

---
+++ tests/scenes/02_subdir/aliases.go
@@ -0,0 +1,10 @@
+package basic
+
+import (
+	"github.com/akm/git-exec/testexec"
+)
+
+var (
+	// run    = testexec.Run
+	stdout = testexec.Stdout
+)
---
+++ tests/scenes/02_subdir/directory_option_test.go
@@ -0,0 +1,26 @@
+package basic
+
+import (
+	"testing"
+
+	"github.com/akm/git-exec/core"
+	"github.com/akm/git-exec/tests/testground"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDirectoryOption(t *testing.T) {
+	defer testground.Setup(t)()
+
+	defer testground.AssertStringNotChanged(t, testground.GitDiff)()
+
+	opts := *core.DefaultOptions
+	opts.Directory = "sub1"
+	err := core.Run(&opts, []string{"make", "add-one", "parent-add-two"})
+	require.NoError(t, err)
+
+	commitMessage := stdout(t, "git", "log", "-1", "--pretty=%B")
+	assert.Equal(t, `🤖 [02_subdir] $ make add-one parent-add-two
+
+`, commitMessage)
+}
